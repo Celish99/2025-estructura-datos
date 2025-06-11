f675c57254e399a2bb0e34d04d28c9d7eb513b5f
Marcelo Caicedo99 <wm.caicedoq@uea.edu.ec>
2025-06-11 09:14:23 -0500

    semana02
    semana02

---
+++ semana02/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("2525 - ESTRUCTURA DE DATOS - UEA");
+
+// Crear un rectangulo con base 5 y Altura 10
+Rectangulo rectangulo = new Rectangulo(5, 10);
+
+// Mostrar la base y Altura
+Console.WriteLine("Base: " + rectangulo.Base);
+Console.WriteLine("Altura: " + rectangulo.Altura);
+
+// calcular y mostrar area y perimetro
+Console.WriteLine("Area: " + rectangulo.CalcularArea());
+Console.WriteLine("Perimetro: " + rectangulo.CalcularPerimetro());
